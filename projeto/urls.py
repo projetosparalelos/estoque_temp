e46b8d9d5aa7d95af69685792b0beeed36fbb52d
rg3915 <rg3915@yahoo.com.br>
2019-02-01 15:51:34 -0200

    Fazer Logout. Close #35

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     path('produto/', include('projeto.produto.urls')),
     path('estoque/', include('projeto.estoque.urls')),
     path('admin/', admin.site.urls),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
